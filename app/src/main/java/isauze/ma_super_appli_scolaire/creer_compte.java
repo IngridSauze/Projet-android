6c5526f91c3ac0c0fea2874a121cadd6c45c90d2
SAUZE Ingrid <sauzei@hostname.iut2.upmf-grenoble.fr>
2018-04-10 15:01:19 +0200

    projet premières vues xml

--- app/src/main/java/isauze/ma_super_appli_scolaire/creer_compte.java
+++ app/src/main/java/isauze/ma_super_appli_scolaire/creer_compte.java
@@ -3,11 +3,11 @@ package isauze.ma_super_appli_scolaire;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class creer_compte extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.creer_compte);
     }
 }
