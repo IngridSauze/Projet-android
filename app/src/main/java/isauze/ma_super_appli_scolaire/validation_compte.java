cb41f9f1f93b967255cebc5647afe13b06c8b96f
SAUZE Ingrid <sauzei@hostname.iut2.upmf-grenoble.fr>
2018-04-13 09:49:25 +0200

    Premières interfaces

---
+++ app/src/main/java/isauze/ma_super_appli_scolaire/validation_compte.java
@@ -0,0 +1,13 @@
+package isauze.ma_super_appli_scolaire;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class validation_compte extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.validation_compte);
+    }
+}
