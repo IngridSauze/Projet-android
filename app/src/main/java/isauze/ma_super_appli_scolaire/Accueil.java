ed58f84aae8adee106d514311f86d12028c78688
SAUZE Ingrid <sauzei@hostname.iut2.upmf-grenoble.fr>
2018-04-24 18:40:27 +0200

    Debut des activités

--- app/src/main/java/isauze/ma_super_appli_scolaire/Accueil.java
+++ app/src/main/java/isauze/ma_super_appli_scolaire/Accueil.java
@@ -1,13 +1,33 @@
 package isauze.ma_super_appli_scolaire;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Accueil extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Surcharge du XML pour créer la hiérarchie des composants graphiques
         setContentView(R.layout.accueil);
+
+        // Récupérer les objets de l'arbre graphique avec leur id
+        Button creer = (Button) findViewById(R.id.a_bouton_nouv_compte) ;
+
+        // Basculer sur la page de création du compte en cas de clic sur le bouton
+        creer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Création de l'intention
+               Intent intent = new Intent(Accueil.this, creer_compte.class) ;
+                startActivity(intent);
+            }
+            }
+
+        ) ;
     }
 }
--- app/src/main/java/isauze/ma_super_appli_scolaire/creer_compte.java
+++ app/src/main/java/isauze/ma_super_appli_scolaire/creer_compte.java
@@ -2,6 +2,10 @@ package isauze.ma_super_appli_scolaire;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class creer_compte extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class creer_compte extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.creer_compte);
+
+        // Récupérer les objets de l'arbre graphique avec leur id
+        Button valider = (Button) findViewById(R.id.c_bouton_valider) ;
+        final TextView prenom = (TextView) findViewById(R.id.c_champ_prenom) ;
+        final TextView nom = (TextView) findViewById(R.id.c_champ_nom) ;
+
+        // Quand l'on clique sur le bouton valider : enregistrement du profil et bascule sur la page de validation
+
+
     }
 }
