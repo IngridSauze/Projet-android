294b5ef1e3b957524a34e49c2982169ff5c9625d
IngridSauze <36404972+IngridSauze@users.noreply.github.com>
2018-06-10 22:55:06 +0200

    Connexion possible

--- app/src/main/java/isauze/ma_super_appli_scolaire/Accueil.java
+++ app/src/main/java/isauze/ma_super_appli_scolaire/Accueil.java
@@ -18,6 +18,7 @@ import java.util.List;
 
 import classes_base.Compte;
 
+import static classes_base.CompteDAO.selectCompte;
 import static classes_base.CompteDAO.selectTous;
 
 public class Accueil extends AppCompatActivity {
@@ -25,7 +26,7 @@ public class Accueil extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        SugarContext.init(this) ;
+        SugarContext.init(this);
 
         // Surcharge du XML pour créer la hiérarchie des composants graphiques
         setContentView(R.layout.accueil);
@@ -37,58 +38,66 @@ public class Accueil extends AppCompatActivity {
 //        schemaGenerator.createDatabase(new SugarDb(getApplicationContext()).getDB());
 
         // Récupérer les valeurs passées par l'intent
-        final int id = getIntent().getIntExtra("ID_COMPTE", 0) ;
+        final int id = getIntent().getIntExtra("ID_COMPTE", 0);
 
         // Récupérer les objets de l'arbre graphique avec leur id
-        final Button creer = (Button) findViewById(R.id.a_bouton_nouv_compte) ;
-        final Button exercices = (Button) findViewById(R.id.a_liste_ex) ;
-        final TextView texte = (TextView) findViewById(R.id.a_choix) ;
+        final Button creer = (Button) findViewById(R.id.a_bouton_nouv_compte);
+        final Button exercices = (Button) findViewById(R.id.a_liste_ex);
+        final TextView texte = (TextView) findViewById(R.id.a_choix);
+        final ListView listeView = (ListView) findViewById(R.id.a_liste);
 
         texte.setVisibility(View.INVISIBLE);
 
         // Si l'utilisateur n'est pas connecté
-        if(id == 0) {
+        if (id == 0) {
             // Rendre invisible le texte de choix du compte
             texte.setVisibility(View.VISIBLE);
             // Récupération de la ListView
-            final ListView listeView = (ListView) findViewById(R.id.a_liste) ;
-            List<Compte> liste_comptes = selectTous() ; // liste de tous les comptes
-            if(!(liste_comptes.isEmpty())) {
-                Compte_adapter adapter = new Compte_adapter(Accueil.this, liste_comptes);
+            List<Compte> liste_comptes = selectTous(); // liste de tous les comptes
+            if (!(liste_comptes.isEmpty())) {
+                final Compte_adapter adapter = new Compte_adapter(Accueil.this, liste_comptes);
                 listeView.setAdapter(adapter);
+                // Rendre les comptes clicables
                 listeView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
+                        String n = adapter.getItem(i).getNom();
+                        String p = adapter.getItem(i).getPrenom();
+                        List<Compte> c = selectCompte(p, n);
+                        if (c.size() == 1) {
+                            int c_id = c.get(0).getId().intValue();
+                            Intent intent = new Intent(Accueil.this, liste_exercices.class);
+                            intent.putExtra("ID_COMPTE", c_id);
+                            startActivity(intent);
+                        }
                     }
                 });
             }
 
-
             // Basculer sur la page de création du compte en cas de clic sur le bouton
             creer.setOnClickListener(new View.OnClickListener() {
-                                         @Override
-                                         public void onClick(View v) {
-                                             // Création de l'intention
-                                             Intent intent = new Intent(Accueil.this, creer_compte.class) ;
-                                             startActivity(intent);
-                                         }
-                                     }
-            ) ;
-            exercices.setVisibility(View.INVISIBLE);
-        }
-        else {
-            exercices.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     // Création de l'intention
-                    Intent intent = new Intent(Accueil.this, liste_exercices.class) ;
-                    intent.putExtra("ID_COMPTE", id) ;
+                    Intent intent = new Intent(Accueil.this, creer_compte.class);
                     startActivity(intent);
                 }
+            });
+            exercices.setVisibility(View.INVISIBLE);
+        }
+        else {
+            creer.setVisibility(View.INVISIBLE); // Rendre la création du compte invisible
+            listeView.setVisibility(View.INVISIBLE);
+            exercices.setOnClickListener(new View.OnClickListener() {
+                                             @Override
+                                             public void onClick(View v) {
+                                                 // Création de l'intention
+                                                 Intent intent = new Intent(Accueil.this, liste_exercices.class);
+                                                 intent.putExtra("ID_COMPTE", id);
+                                                 startActivity(intent);
+                                             }
                                          }
             );
-            creer.setVisibility(View.INVISIBLE);
         }
 
     }
