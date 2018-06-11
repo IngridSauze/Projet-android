63966d8f9539d94f6250e8920c5e55b334c0e547
IngridSauze <36404972+IngridSauze@users.noreply.github.com>
2018-06-11 22:29:11 +0200

    Culture_g finie

--- app/src/main/java/classes_base/Resultat.java
+++ app/src/main/java/classes_base/Resultat.java
@@ -9,15 +9,22 @@ import com.orm.SugarRecord;
 public class Resultat extends SugarRecord {
     private long compte ;
     private int addition_table ;
+    private int culture_g ;
 
     // Constructeurs
     public Resultat() {
     }
 
-    public Resultat(String message, long compte, int addition_table) {
-        if(message.equals("addition_table")) {
+    public Resultat(String message, long compte, int i) {
+        if (message.equals("addition_table")) {
             this.compte = compte ;
-            this.addition_table = addition_table ;
+            this.addition_table = i ;
+            this.culture_g = 0 ;
+        }
+        if (message.equals("culture_g")) {
+            this.compte = compte ;
+            this.addition_table = 0 ;
+            this.culture_g = i ;
         }
     }
 
@@ -25,7 +32,15 @@ public class Resultat extends SugarRecord {
         return addition_table ;
     }
 
+    public  int getCulture() {
+        return culture_g ;
+    }
+
     public void setAddition_table(int t) {
         this.addition_table = t ;
+    }
+
+    public void setCulture_g(int t) {
+        this.culture_g = t ;
     }
 }
--- app/src/main/java/isauze/ma_super_appli_scolaire/culture_generale.java
+++ app/src/main/java/isauze/ma_super_appli_scolaire/culture_generale.java
@@ -1,5 +1,6 @@
 package isauze.ma_super_appli_scolaire;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,7 +9,12 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import java.util.List;
 import java.util.Random;
+
+import classes_base.Resultat;
+
+import static classes_base.ResultatDAO.selectRes;
 
 public class culture_generale extends AppCompatActivity {
     private String[] questions = {"Quelle est la date de la bataille de Marignan ?",
@@ -24,22 +30,22 @@ public class culture_generale extends AppCompatActivity {
             "Quel est la date de la révolution française ?",
             "Comment s'appelle l'hymne national français ?",
             "Quel est le fondateur de la Cinquième République ?"
-    } ;
+    };
 
     private String[] reponse = {
             "1515", "Pérou", "Berlin", "Ni l'un ni l'autre", "Auguste", "Louis-Philippe Ier", "Roi de Rome", "Smenkharê",
             "Smenkharê", "5", "1789", "La Marseillaise", "Charles de Gaulle"
-    } ;
+    };
 
     private String[] faux1 = {
             "1456", "Italie", "Paris", "Les plumes", "Jules César", "Louis XVI", "Prince de Padoue", "Toutankhamon",
             "Akhénaton", "15", "1790", "God Save the Queen", "Georges Pompidou"
-    } ;
+    };
 
     private String[] faux2 = {
             "1517", "Pologne", "Oslo", "Le plomb", "Romulus Augustule", "Louis XVIII", "Prince de Corse", "Ramsès XVII",
             "Toutencarton", "2", "1678", "Le Chant des Partisans", "Vincent Auriol"
-    } ;
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,51 +56,98 @@ public class culture_generale extends AppCompatActivity {
         final int id = getIntent().getIntExtra("ID_COMPTE", 0);
 
         // Récupérer les objets de l'arbre graphique avec leur id
-        final Button valider = (Button) findViewById(R.id.g_exercice);
+        final Button valider = (Button) findViewById(R.id.g_valider);
         final Button exercices = (Button) findViewById(R.id.g_exercice);
         final Button accueil = (Button) findViewById(R.id.g_b_accueil);
-        final TextView texte = (TextView) findViewById(R.id.g_texte_accueil_ac) ;
-        final RadioGroup groupe = (RadioGroup) findViewById(R.id.g_groupe) ;
-        final RadioButton radio1 = (RadioButton) findViewById(R.id.g_radio1) ;
-        final RadioButton radio2 = (RadioButton) findViewById(R.id.g_radio2) ;
-        final RadioButton radio3 = (RadioButton) findViewById(R.id.g_radio3) ;
+        final TextView texte = (TextView) findViewById(R.id.g_texte_accueil_ac);
+        final RadioGroup groupe = (RadioGroup) findViewById(R.id.g_groupe);
+        final RadioButton radio1 = (RadioButton) findViewById(R.id.g_radio1);
+        final RadioButton radio2 = (RadioButton) findViewById(R.id.g_radio2);
+        final RadioButton radio3 = (RadioButton) findViewById(R.id.g_radio3);
 
-        groupe.addView(radio1); ;
-//        groupe.addView(radio2) ;
-//        groupe.addView(radio3) ;
-//
-//        exercices.setVisibility(View.INVISIBLE);
-//        accueil.setVisibility(View.INVISIBLE);
-//
-//        // Variables de contrôle
-//        int nb_question = 0 ;
-//
-//        // Mise en place des quetions
-//        for(int i = 0 ; i < 10 ; i++) {
-//            // Hasard
-//            Random r_question = new Random();
-//            final int valeur = 0 + r_question.nextInt(questions.length - 0) ;
-//
-//            texte.setText(questions[valeur]);
-//            radio1.setText(reponse[valeur]);
-//            radio2.setText(faux1[valeur]);
-//            radio3.setText(faux2[valeur]);
-//
-//            valider.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    int erreurs = 0 ;
-//                    RadioButton r = (RadioButton) groupe.getChildAt(groupe.getCheckedRadioButtonId()) ;
-//                    if (!(r.getText().equals(reponse[valeur])))  {
-//                        erreurs++ ;
-//                    }
-//                }
-//        }) ;
-//
-//    }
-//
-//        exercices.setVisibility(View.VISIBLE);
-//        accueil.setVisibility(View.VISIBLE);
-//        valider.setVisibility(View.INVISIBLE);
-}
+        exercices.setVisibility(View.INVISIBLE);
+        accueil.setVisibility(View.INVISIBLE);
+
+        // Mise en place des questions
+        // Hasard
+        Random r_question = new Random();
+        int valeur = 0 + r_question.nextInt(questions.length - 0);
+
+        texte.setText(questions[valeur]);
+        radio1.setText(reponse[valeur]);
+        radio2.setText(faux1[valeur]);
+        radio3.setText(faux2[valeur]);
+
+        valider.setOnClickListener(new View.OnClickListener() {
+            int erreurs = 0;
+            int nb_question = 0;
+
+            @Override
+            public void onClick(View v) {
+                RadioButton r = (RadioButton) groupe.getChildAt(groupe.getCheckedRadioButtonId());
+                if (nb_question < 10) {
+                    if (!(radio1.isChecked())) {
+                        erreurs++;
+                    }
+                    Random r_question = new Random();
+                    int valeur = 0 + r_question.nextInt(questions.length - 0);
+                    texte.setText(questions[valeur]);
+                    radio1.setText(reponse[valeur]);
+                    radio2.setText(faux1[valeur]);
+                    radio3.setText(faux2[valeur]);
+                    nb_question++;
+                }
+                else {
+                    groupe.setVisibility(View.INVISIBLE);
+                    exercices.setVisibility(View.VISIBLE);
+                    accueil.setVisibility(View.VISIBLE);
+                    valider.setVisibility(View.INVISIBLE);
+
+                    List<Resultat> res1 = selectRes(id) ;
+                    if(res1.size()== 1) {
+                        Resultat res = res1.get(0) ; // Récupération du résultat de l'utilisateur
+                        if (res.getCulture() >= 10-erreurs) {
+                            texte.setText("Votre record à cet exercice est de " + res.getCulture() + ".");
+                        }
+                        else {
+                            res.setCulture_g(10-erreurs);
+                            res.save() ;
+                            texte.setText("Votre record à cet exercice est de " + res.getCulture() + ".");
+                        }
+                    }
+                    else if(res1.size()== 0) {
+                        Resultat res2 = new Resultat("culture_g", (long) id, 10-erreurs) ;
+                        res2.save();
+                        texte.setText("Votre record à cet exercice est de " + res2.getCulture() + ".");
+                    }
+                    else {
+                        texte.setText("Impossible d'afficher le score.");
+                    }
+                }
+            }
+        });
+
+        accueil.setOnClickListener(new View.OnClickListener() {
+                                       @Override
+                                       public void onClick(View v) {
+                                           Intent intent = new Intent(culture_generale.this, Accueil.class) ;
+                                           intent.putExtra("ID_COMPTE", id) ;
+                                           intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP) ;
+                                           startActivity(intent);
+                                       }
+                                   }
+        );
+
+        exercices.setOnClickListener(new View.OnClickListener() {
+                                         @Override
+                                         public void onClick(View v) {
+                                             // Création de l'intention
+                                             Intent intent = new Intent(culture_generale.this, liste_exercices.class) ;
+                                             intent.putExtra("ID_COMPTE", id) ;
+                                             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP) ;
+                                             startActivity(intent);
+                                         }
+                                     }
+        );
+    }
 }
